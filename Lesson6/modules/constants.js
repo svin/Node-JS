225552a823e8e32a9cf82f0e40a60ed5f4f9cb01
svin <keddie86@gmail.com>
2016-07-13 18:46:13 +0300

    lesson 6

---
+++ Lesson6/modules/constants.js
@@ -0,0 +1,12 @@
+GLOBAL.BASE_PATH = "c:\\mashu";
+GLOBAL.ADMIN_USER = "root";
+GLOBAL.ADMIN_PASS = "1234";
+
+
+//read only methods
+// CONSTANTS.ADMIN_USER() 
+GLOBAL.CONSTANTS = {
+    ADMIN_USER: function() {
+        return "root";
+    }
+}
---
+++ Lesson6/modules/myWS.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var htmlencode = require('htmlencode');
+
+
+//configure empty http server
+var httpServer = http.createServer(function(rq, rs) {});
+httpServer.listen(1337, function() {
+    console.log('server started on 1337');
+});
+
+//configure the WS server to attach itself into the http server
+var webSocketServer = require('websocket').server;
+var ws = new webSocketServer({
+    "httpServer": httpServer
+});
+
+var clients = [];
+ws.on('request', function(req) {
+    //this hold the client connection
+    var connection = req.accept(null, req.origin);
+    //store the connection into clients array
+    clients.push(connection);
+    //send response
+    connection.sendUTF("Conn established");
+    //take care on clients messages
+    connection.on('message', function(msg) {
+        for (var i = 0; i < clients.length; i++) {
+            //get one of the clients connecion
+            var usercon = clients[i];
+            usercon.sendUTF(htmlencode.htmlEncode(msg.utf8Data));
+
+        }
+    })
+
+})
